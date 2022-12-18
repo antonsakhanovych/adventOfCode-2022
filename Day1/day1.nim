2506c0817567fa789f0af96c10745ef90315367b
Anton Sakhanovych <antonsakhanovych@gmail.com>
2022-12-18 12:37:05 +0100

    restructured and added solution for day1 part2

--- Day1/day1.nim
+++ Day1/day1.nim
@@ -1,17 +1,49 @@
 import strutils
 
-var 
-    biggest: int = 0
-    sum: int = 0
+proc fileParser(fileName: string): seq[int] =
+    var 
+        calories: seq[int] = @[]
+        sum: int = 0
 
-for line in lines("input.txt"):
-    if line == "":
-        if sum>biggest:
-            biggest = sum
+    for line in lines(filename):
+        if line == "":
+            calories.add(sum)
             sum = 0
         else:
-            sum = 0
-        continue
-    sum = sum + parseInt(line)
-    
-echo biggest
+            sum = sum + parseInt(line)
+    return calories
+
+proc getBiggest(calories: seq[int]): (int, int) = 
+    var 
+        biggest: int = 0
+        ind: int = 0
+    for i, cal in calories:
+        if cal > biggest:
+            biggest = cal
+            ind = i
+    return (ind, biggest)
+
+proc part1() =
+    let 
+        calories: seq[int] = fileParser("input.txt")
+        biggest: (int, int) = getBiggest(calories)
+    echo "Answer for the first part: ", biggest[1]
+
+proc sumCalories(calories: openArray[int]): int =
+    for el in calories:
+        result += el
+
+proc part2() =
+    var 
+        calories: seq[int] = fileParser("input.txt")
+        threeBiggest: array[3, int] = [0, 0, 0]
+    for ind, _ in threeBiggest:
+        let res = getBiggest(calories)
+        threeBiggest[ind] = res[1]
+        calories.del(res[0])
+    echo "Answer for the second part: ", sumCalories(threeBiggest)
+        
+
+if isMainModule:
+    part1()
+    part2()
